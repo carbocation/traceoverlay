978f77b5432b5ae6e39409114910863a9fd0bf4a
James Pirruccello <james@carbocation.com>
2020-05-28 19:17:33 -0400

    Set the default brush size to 2, since this allows fills.

--- web/templates/static/js/traceoverlay.js
+++ web/templates/static/js/traceoverlay.js
@@ -16,7 +16,7 @@ var saveAlpha = 255;
 
 // Brush variables
 var brush = "exact";
-var defaultBrushSize = 1;
+var defaultBrushSize = 2;
 var brushSize = defaultBrushSize;
 var brushColor = "#ff0000"; //"rgba(255, 0, 0, 1)"; //"#FF0000"; //{r:0xff, g:0x00, b:0x00, a:0xff}; //"#FF0000";
 
