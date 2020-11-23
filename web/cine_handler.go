b6b6b6e06b46a51b6f14f4934fd2fb6617a29f7e
James Pirruccello <james@carbocation.com>
2020-11-23 13:12:50 -0500

    Use the new ImageGrid method from bulkprocess.

--- web/cine_handler.go
+++ web/cine_handler.go
@@ -2,9 +2,6 @@ package main
 
 import (
 	"fmt"
-	"image"
-	"image/color"
-	"image/draw"
 	"image/gif"
 	"net/http"
 	"strconv"
@@ -88,7 +85,7 @@ func (h *handler) TraceOverlayCINE(w http.ResponseWriter, r *http.Request) {
 	// Total hack to get the SAX images to display as a grid instead of
 	// sequentially
 	if showAll {
-		newDicomNames, newImageMap, err := ImageGrid(dicomNames, imageMap, series, seriesAlignment, 50, 4)
+		newDicomNames, newImageMap, err := bulkprocess.ImageGrid(dicomNames, imageMap, series, seriesAlignment, 50, 4)
 		if err != nil {
 			HTTPError(h, w, r, fmt.Errorf("%s:%s: %v", zipFile, series, err))
 			return
@@ -111,105 +108,4 @@ func (h *handler) TraceOverlayCINE(w http.ResponseWriter, r *http.Request) {
 		HTTPError(h, w, r, fmt.Errorf("%s:%s: %v", zipFile, series, err))
 		return
 	}
-}
-
-// ImageGrid is a hack that converts a SAX stack (structured as all 50
-// timepoints for series 1, all 50 timepoints for series 2, etc) into a grid
-// that simultaneously shows each series side-by-side. It highlights the active
-// series based on a seriesAlignment slice that is paired 1:1 with the
-// dicomNames slice.
-func ImageGrid(dicomNames []string, imageMap map[string]image.Image, series string, seriesAlignment []string, imagesInCardiacCycle, Ncols int) ([]string, map[string]image.Image, error) {
-	Nseries := len(imageMap) / imagesInCardiacCycle
-	Nrows := Nseries / Ncols
-	if Nseries%Ncols != 0 {
-		Nrows++
-	}
-	maxWidth := -1
-	maxHeight := -1
-	for _, img := range imageMap {
-		if x := img.Bounds().Dx(); x > maxWidth {
-			maxWidth = x
-		}
-		if y := img.Bounds().Dy(); y > maxWidth {
-			maxHeight = y
-		}
-	}
-
-	newDicomNames := make([]string, 0, imagesInCardiacCycle)
-	newImageMap := make(map[string]image.Image)
-
-	// For each part of the cardiac cycle, draw our images, for a total of
-	// imagesInCardiacCycle images
-	for i := 0; i < imagesInCardiacCycle; i++ {
-
-		r := image.Rect(0, 0, Ncols*maxWidth, Nrows*maxHeight)
-		thisImg := image.NewRGBA(r)
-		// Set a black background
-		draw.Draw(thisImg, thisImg.Bounds(), &image.Uniform{color.Black}, image.ZP, draw.Src)
-
-		seriesCounter := 0
-
-		// We know that we need to draw Nseries images onto this template
-	ImageGridLoop:
-		for row := 0; row < Nrows; row++ {
-			for col := 0; col < Ncols; col++ {
-
-				imageID := i + imagesInCardiacCycle*(row*Ncols+col)
-				if imageID > len(dicomNames) {
-					continue
-				}
-
-				pane, exists := imageMap[dicomNames[imageID]]
-				if !exists {
-					break
-				}
-
-				startX := col * maxWidth
-				startY := row * maxHeight
-
-				drawRect := image.Rect(startX, startY, startX+pane.Bounds().Dx(), startY+pane.Bounds().Dy())
-
-				// Draw the pane into the master image for this timepoint at its designated spot
-				draw.Draw(thisImg, drawRect, pane, image.ZP, draw.Src)
-
-				// Highlight the active pane
-				if len(seriesAlignment) > imageID && seriesAlignment[imageID] == series {
-					borderColor := color.RGBA{R: 128, G: 128, B: 128, A: 255}
-
-					// Top
-					innerRect := drawRect
-					innerRect.Max.Y = innerRect.Min.Y + 2
-					draw.Draw(thisImg, innerRect, &image.Uniform{borderColor}, image.ZP, draw.Src)
-
-					// Bottom
-					innerRect = drawRect
-					innerRect.Min.Y = innerRect.Max.Y - 2
-					draw.Draw(thisImg, innerRect, &image.Uniform{borderColor}, image.ZP, draw.Src)
-
-					// Left
-					innerRect = drawRect
-					innerRect.Max.X = innerRect.Min.X + 2
-					draw.Draw(thisImg, innerRect, &image.Uniform{borderColor}, image.ZP, draw.Src)
-
-					// Right
-					innerRect = drawRect
-					innerRect.Min.X = innerRect.Max.X - 2
-					draw.Draw(thisImg, innerRect, &image.Uniform{borderColor}, image.ZP, draw.Src)
-				}
-
-				seriesCounter++
-
-				// Account for jagged series counts
-				if seriesCounter >= Nseries {
-					break ImageGridLoop
-				}
-
-			}
-		}
-
-		newDicomNames = append(newDicomNames, strconv.Itoa(i))
-		newImageMap[strconv.Itoa(i)] = thisImg
-	}
-
-	return newDicomNames, newImageMap, nil
 }
